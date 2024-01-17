e3578b13c6a4ab513dc5f7fc274a90e8d18f9b8b
p-remy <152425528+p-remy@users.noreply.github.com>
2024-01-17 13:21:46 +0100

    Create mongo_dump_pipeline.py

---
+++ mongo_dump_pipeline.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+import json
+from pymongo.server_api import ServerApi
+
+# Read the JSON file
+with open("flights.json") as file:
+    data = json.load(file)
+
+load_dotenv()
+uri = os.environ.get("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Select the database
+db = client["meteor"]
+
+# Select the collection
+collection = db["real time flights"]
+
+# Insert the JSON data into the collection
+collection.insert_many(data)
