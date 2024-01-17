b948d5eb68f59b7c4c52355cda2226456ceaee43
p-remy <152425528+p-remy@users.noreply.github.com>
2024-01-17 12:30:50 +0100

    Create airlabs-schedules.py

---
+++ scripts/airlabs-schedules.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+import requests
+from pprint import pprint
+import json
+
+load_dotenv()
+api_key = os.environ.get("AIRLABS_KEY")
+
+params = {"api_key": api_key}
+
+api_result = requests.get("https://airlabs.co/api/v9/schedules", params)
+
+api_response = api_result.json()["response"]
+
+with open("schedules.json", "w") as file:
+    json.dump(api_response, file, indent=4)
+
+pprint(api_response)
