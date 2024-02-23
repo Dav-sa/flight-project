ebd5f7f8082b1f5d217290b68d1fe4d4718d29ce
p-remy <152425528+p-remy@users.noreply.github.com>
2024-02-23 12:31:50 +0100

    Update setup.sh

--- processing_api_server/setup.sh
+++ processing_api_server/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-test_folders="airlines_api airlines_ml"
+Dockerfile_folders="airlines_api airlines_ml"
 
-for i in $test_folders
+for i in $Dockerfile_folders
   do
     docker image build $i -t $i:latest
   done
 
 docker compose up -d
-docker run --rm --volume /home/remy/Airlines/shared:/home/shared -d airlines_ml python3 /home/shared/train_ml.py
-crontab -l | { cat; echo "18 */1 * * * docker run --rm --volume /home/remy/Airlines/shared:/home/shared -d airlines_ml python3 /home/shared/train_ml.py"; } | crontab -
+docker run --rm --volume $PWD/shared:/home/shared -d airlines_ml python3 /home/shared/train_ml.py
+crontab -l | { cat; echo "18 */1 * * * docker run --rm --volume $PWD/shared:/home/shared -d airlines_ml python3 /home/shared/train_ml.py"; } | crontab -
