7a09a689769eb3a44252ae2b79be33bf9d3cc146
p-remy <152425528+p-remy@users.noreply.github.com>
2024-02-29 17:38:39 +0100

    Update setup.sh

--- fetching_storing_server/setup.sh
+++ fetching_storing_server/setup.sh
@@ -4,7 +4,4 @@ docker image build mongo_dump -t mongo_dump:latest
 
 docker-compose up -d
 
-sleep 60
-
-docker run --rm --volume $PWD/shared:/home/shared -d mongo_dump
-crontab -l | { cat; echo "09 */1 * * * docker run --rm --volume $PWD/shared:/home/shared -d mongo_dump"; } | crontab -
+crontab -l | { cat; echo "09 */1 * * * docker-compose -f $PWD/docker-compose.yml up mongo_dump"; } | crontab -
